526b97cc5691a72d76e72f5c815e9ca9105ec569
Vitor-costa-diniz <costavitor.8@hotmail.com>
2022-12-07 15:06:07 -0300

    Refazendo AppBar

--- movie/lib/home_page.dart
+++ movie/lib/home_page.dart
@@ -12,24 +12,58 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leadingWidth: 200,
-        backgroundColor: Color(0xffA87FF3),
-        title: Row(
-          children: [
-            Image.asset('images/Logo.png'),
-            Container(
-              padding: EdgeInsets.only(left: 10),
-              child: Text(
-                'MovieDB',
-                style: GoogleFonts.poppins(
-                  color: Color(0xffFFFFFF),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Container(
+      backgroundColor: Colors.black,
+      // appBar: AppBar(
+      //   leadingWidth: 200,
+      //   backgroundColor: Color(0xffA87FF3),
+      //   title: Row(
+      //     children: [
+      //       Image.asset('images/Logo.png'),
+      //       Container(
+      //         padding: EdgeInsets.only(left: 10),
+      //         child: Text(
+      //           'MovieDB',
+      //           style: GoogleFonts.poppins(
+      //             color: Color(0xffFFFFFF),
+      //             fontWeight: FontWeight.bold,
+      //             fontSize: 20,
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //           padding: EdgeInsets.only(left: 14),
+      //           height: MediaQuery.of(context).size.height * 0.042,
+      //           width: MediaQuery.of(context).size.width * 0.48,
+      //           child: TextField(
+      //             onChanged: (txt) {
+      //               print(txt);
+      //             },
+      // keyboardType: TextInputType.emailAddress,
+      // decoration: InputDecoration(
+      //   filled: true,
+      //   fillColor: Color(0xffE9DFFC),
+      //   prefixIcon: Icon(Icons.search, color: Colors.grey),
+      //   border: OutlineInputBorder(
+      //     borderRadius: BorderRadius.all(
+      //       Radius.circular(15),
+      //     ),
+      //   ),
+      // ),
+      // ))
+      //     ],
+      //   ),
+      //   automaticallyImplyLeading: false,
+      // ),
+      // endDrawer: Container(
+      //   child: Drawer(),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Color(0xffA87FF3),
+            leading: Image.asset('images/Logo.png', scale: 1),
+            actions: [
+              Container(
                 padding: EdgeInsets.only(left: 14),
                 height: MediaQuery.of(context).size.height * 0.042,
                 width: MediaQuery.of(context).size.width * 0.48,
@@ -48,20 +82,17 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                   ),
-                ))
-          ],
-        ),
-        automaticallyImplyLeading: false,
-      ),
-      endDrawer: Container(
-        child: Drawer(),
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.black,
-        ),
+                ),
+              ),
+              IconButton(
+                color: Colors.white,
+                iconSize: 30,
+                icon: Icon(Icons.menu_outlined),
+                onPressed: () {},
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
